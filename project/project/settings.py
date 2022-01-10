1e2254317a4238328c94fcba59261b524a5ad0c8
Timur <timur32240@gmail.com>
2022-01-10 22:04:42 +0700

    commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_FILES = (PROJECT_ROOT, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
