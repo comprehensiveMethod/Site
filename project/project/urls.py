cd22ac8d79dbeadd7ce03d849ae6892fc057a3b0
vladikus-code <vladikus_man@bk.ru>
2022-01-11 23:03:37 +0700

    first css

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main import views
 
 urlpatterns = [
-    path('forum/', include('forum.urls') ),
+    path('', views.main_page),
+    path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
 ]
