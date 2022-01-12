67102351e3f690c627a296264eb931fbd12a0351
vladikus-code <vladikus_man@bk.ru>
2022-01-12 16:20:26 +0700

    test mainPageStyle.css

--- project/project/settings.py
+++ project/project/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, '../apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
