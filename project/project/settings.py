21e8aaea1d56b302ff1f5cd313a8657debab392d
Timur <timur32240@gmail.com>
2022-01-14 02:57:43 +0700

    oops, deleted settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
